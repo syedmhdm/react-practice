45aca59a6d0f910d0696553fb3a0af63bad12268
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-24 02:03:02 +0530

    consolo log doesn't return any thing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  for (var i = 1; i <= 3; i++) {
-    setTimeout(() => {
-      console.log(i);
-    }, 1000);
+  // for (var i = 1; i <= 3; i++) {
+  //   setTimeout(() => {
+  //     console.log(i);
+  //   }, 1000);
+  // }
+
+  function left() {
+    return console.log("Hello");
   }
+
+  function right() {
+    return console.log(true);
+  }
+
+  console.log(left() && right());
+
+  // if (undefined) {
+  //   console.log("truthy");
+  // } else {
+  //   console.log("fals");
+  // }
 
   return (
     <div className='App'>
