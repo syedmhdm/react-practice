a8d914c44f95d848ac859b58abe26eca896cd52c
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-01-24 01:57:01 +0530

    js nuances

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  for (var i = 1; i <= 3; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, 1000);
+  }
+
   return (
     <div className='App'>
-      <div>{console.log("All unusual ways how react works")}</div>
+      {/* <div>{console.log("All unusual ways how react works")}</div> */}
     </div>
   );
 }
