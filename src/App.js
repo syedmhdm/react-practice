3625685c9bb9ae776963ed18022cb30068cb393f
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2024-02-25 17:07:25 +0530

    react practice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-/*
+import { Fragment, useState } from "react"; /*
 1. create a custom react hook to fetch users from an API (https://randomuser.me/api) and display their picture and name on the page one
 2. It must return the list of users, the current user, a function to fetch the next user and a function to move back to the previous user
 3. Test the custom react hook in the index.js (tsx - later)
 */
 
 import { useRandomUsers } from "./hooks/useRandomUsers";
+import Child from "./Child";
 
 function App() {
   const [allUsers, currentUser, prevUser, nextUser, isLoading] =
     useRandomUsers();
 
+  // const [pCount, setPCount] = useState()
+
+  function pCount(pCount) {
+    console.log("parent comp: " + pCount);
+  }
+
   return (
     <div>
       {allUsers.map((el, i) => (
-        <div key={i}>{`${el.name.title} ${el.name.first} ${el.name.last}`}</div>
+        <Fragment
+          key={i}
+        >{`${el.name.title} ${el.name.first} ${el.name.last}`}</Fragment>
       ))}
       {!isLoading ? (
         <div>
@@ -23,7 +32,7 @@ function App() {
       ) : (
         <p>Loading...</p>
       )}
-
+      <Child pCount={pCount} />
       <button onClick={prevUser}>Prev User</button>
       <button onClick={nextUser}>Next User</button>
     </div>
---
+++ src/Child.js
@@ -0,0 +1,31 @@
+import { memo, useEffect, useRef, useState } from "react";
+import styles from "./Child.module.css";
+
+function Child({ pCount }) {
+  // let count = useRef(0);
+  const [count, setCount] = useState(0);
+  // const [_, setRerender] = useState(false);
+
+  function handleButtonClick() {
+    // count.current = count.current + 1;
+    // console.log(count);
+    setCount((prev) => {
+      const helper = prev + 1;
+      pCount(helper);
+      return helper;
+    });
+  }
+
+  return (
+    <div>
+      <button className={styles.childbutton} onClick={handleButtonClick}>
+        change
+      </button>
+      {/* <button onClick={() => setRerender((prev) => !prev)}>rerender</button> */}
+      {/* {count.current} */}
+      {count}
+    </div>
+  );
+}
+
+export default memo(Child);
